783e075dc024a021d8ec9a5a26bf12f99e195d95
Pablo May <pablojesusmay@gmail.com>
2021-01-12 11:11:31 -0500

    Fix to plugin misconfiguration

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
 const plugin = require("tailwindcss/plugin");
 
-module.exports = {
-  plugins: [
-    plugin(function ({ addVariant, e }) {
-      addVariant("empty", ({ modifySelectors, separator }) => {
-        modifySelectors(({ className }) => {
-          return `.${e(`empty${separator}${className}`)}:empty`;
-        });
-      });
-    }),
-  ],
-};
+module.exports = plugin(function ({ addVariant, e }) {
+  addVariant("empty", ({ modifySelectors, separator }) => {
+    modifySelectors(({ className }) => {
+      return `.${e(`empty${separator}${className}`)}:empty`;
+    });
+  });
+});
